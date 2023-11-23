1e72e8c0bf8637570dc5711b65ae3d95051dc38d
Berenice Valle <bere.s.valle@hotmail.com>
2023-11-23 16:36:00 -0300

    add mais paginas

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 //Ativando o meu mobile
 
 function mostrarMenu(){
-    $('nav#nav-esquerda ul.menu-principal').css('display', 'flex');
-    $('nav#nav-esquerda ul.menu-principal').addClass('animate__animated animate__fadeInRigth animate__slow');
+    $('nav.nav-esquerda ul.menu-principal').css('display', 'flex');
+    $('nav.nav-esquerda ul.menu-principal').addClass('animate__animated animate__fadeInRigth animate__slow');
 
-    $('nav#nav-esquerda ul#icone-menu li#menu').css('display', 'none');
-    $('nav#nav-esquerda ul#icone-menu li#menuX').css('display', 'flex');
+    $('nav.nav-esquerda ul.icone-menu li.menu').css('display', 'none');
+    $('nav.nav-esquerda ul.icone-menu li.menuX').css('display', 'flex');
 }
 
 function esconderMenu(){
-    $('nav#nav-esquerda ul.menu-principal').css('display', 'none');
+    $('nav.nav-esquerda ul.menu-principal').css('display', 'none');
 
-    $('nav#nav-esquerda ul#icone-menu li#menu').css('display', 'flex');
-    $('nav#nav-esquerda ul#icone-menu li#menuX').css('display', 'none');
+    $('nav.nav-esquerda uli.cone-menu li.menu').css('display', 'flex');
+    $('nav.nav-esquerda ul.icone-menu li.menuX').css('display', 'none');
 }
 
 let controle = true;
  
-$('nav#nav-esquerda ul#icone-menu').click(function(){
+$('nav.nav-esquerda ul.icone-menu').click(function(){
     if(controle == true){
         mostrarMenu();
         controle = false;

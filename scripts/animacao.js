ee62b74d3f757f896f68e94ac37ba5bd265d1485
Berenice Valle <bere.s.valle@hotmail.com>
2024-01-23 12:19:22 -0300

    mais alguma alterações no site a pedido do cliente

--- scripts/animacao.js
+++ scripts/animacao.js
@@ -1,17 +1,13 @@
 /*-------  Ações de navegação hader-nav-menu  -------*/
 
-$('header nav#nav-esquerda ul.menu-principal li:eq(0)').click(function(){
+$('header nav.nav-esquerda ul.menu-principal li:eq(0)').click(function(){
     $('html, body').animate({scrollTop:$('header').offset().top}, 1000)
 })
 
-$('header nav#nav-esquerda ul.menu-principal li:eq(1)').click(function(){
-    $('html, body').animate({scrollTop:$('section.sobre').offset().top}, 1000)
+.$('header nav.nav-esquerda ul.menu-principal li:eq(1)').click(function(){
+    $('html, body').animate({scrollTop:$('section.tratamento').offset().top}, 1000)
 })
 
-$('header nav#nav-esquerda ul.menu-principal li:eq(2)').click(function(){
-    $('html, body').animate({scrollTop:$('section.solucao').offset().top}, 1000)
-})
-
-$('header nav#nav-esquerda ul.menu-principal li:eq(3)').click(function(){
-    $('html, body').animate({scrollTop:$('section.make').offset().top}, 1000)
+.$('header nav.nav-esquerda ul.menu-principal li:eq(2)').click(function(){
+    $('html, body').animate({scrollTop:$('section.sobre-mim').offset().top}, 1000)
 })
